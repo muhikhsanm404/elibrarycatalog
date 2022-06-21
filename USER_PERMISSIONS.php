f2a8fe0c05ab764be2e8d0d37b5643bd8b4fe6b3
Arjun Singh <admin@arjunsingh.com.np>
2022-06-21 08:48:03 +0545

    minor fixes

--- USER_PERMISSIONS.php
+++ USER_PERMISSIONS.php
@@ -4,18 +4,21 @@ function is_authenticated() {
     if (isset($_SESSION['id'])) {
         return true;
     }
+    return false;
 }
 
 function is_admin() {
     if (isset($_SESSION['id']) && $_SESSION['is_admin'] == 1) {
         return true;
     }
+    return false;
 }
 
 function is_user() {
     if (isset($_SESSION['id']) && $_SESSION['is_admin'] == 0) {
         return true;
     }
+    return false;
 }
 
 ?>
